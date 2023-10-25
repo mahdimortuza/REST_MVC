cdb8fc0d9cddbd3480d71689526f513741587bb9
mahdimortuza <mahdi.webx@gmail.com>
2023-10-25 14:23:35 +0600

    handling server error

--- app.js
+++ app.js
@@ -10,9 +10,17 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/./views/index.html");
 });
 
+// route not found error
 app.use((req, res, next) => {
   res.status(404).json({
     message: "Route not found",
+  });
+});
+
+// handling server error
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: "Something broke",
   });
 });
 module.exports = app;
